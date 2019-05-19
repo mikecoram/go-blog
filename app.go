658189992ab40dce60e5ccff4a5b85368c26eb22
mikecoram <mikecoram96@gmail.com>
2019-05-19 13:16:42 +0100

    Render html template

--- app.go
+++ app.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello\n")
+// Page object
+type Page struct {
+	Title string
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("views/post.html")
+	t.Execute(w, &Page{Title: "My Title"})
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/post/", postHandler)
 	fmt.Printf("Listening on localhost:8080...\n")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
